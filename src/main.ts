d355e2a8ce45de9ff29bcc1f82f3cf49f8c098b1
Tran Thieu <thieuhaitam@gmail.com>
2024-01-22 23:52:21 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config = new DocumentBuilder()
     .setTitle('E-Learning')
     .setDescription('E learning API description')
